d30a24e1bc84c180df7e59e0e3accfe3fca64af2
ThCC <thiago.decastro2@gmail.com>
2017-04-20 11:05:58 -0300

    New release
    0.1.11

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='postman_client',
-    version='0.1.10',
+    version='0.1.11',
     install_requires=[
         'requests==2.11.0',
         'simplejson==3.6.4',
